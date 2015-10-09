152392312ee4a89148f9d33a6cc68fdcb47ff9f6
zjicmDarkWing <1126955201@qq.com>
2015-10-10 07:39:11 +0800

    sntp client

---
+++ 1/1_12_sntp_client.py
@@ -0,0 +1,25 @@
+__author__ = 'DarkWing'
+
+import socket
+import struct
+import sys
+import time
+
+NTP_SERVER = "0.uk.pool.ntp.org"
+TIME1970 = 2208988800L
+
+def sntp_client():
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    data = "\x1b" + 47 * "\0"
+    #client.connect((NTP_SERVER,123))
+    client.sendto(data,(NTP_SERVER,123))
+    data,address = client.recvfrom(1024)
+
+    if data:
+        print "Response received from:",address
+    t = struct.unpack("!12I",data)[10]
+    t -= TIME1970
+    print "\tTime=%s" %time.ctime(t)
+
+if __name__ == '__main__':
+    sntp_client()
