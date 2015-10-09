732f7f2c1857573e4cd4cc073bc7263856a34839
zjicmDarkWing <1126955201@qq.com>
2015-10-09 22:56:08 +0800

    socket modes

---
+++ 1/1_9_socket_modes.py
@@ -0,0 +1,18 @@
+__author__ = 'miao'
+
+import socket
+
+def test_socket_modes():
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.setblocking(1)
+    s.settimeout(0.5)
+    s.bind(("127.0.0.1",7777))
+
+    socket_address = s.getsockname()
+    print "Trivial Server launched on socket: %s" %str(socket_address)
+
+    while(1):
+        s.listen(1)
+
+if __name__ == '__main__':
+    test_socket_modes()
