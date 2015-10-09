305f7ec89c317da1d966a563ac953042dba1ff0a
zjicmDarkWing <1126955201@qq.com>
2015-10-10 07:30:31 +0800

    ntp client

---
+++ 1/1_11_print_machine_time.py
@@ -0,0 +1,12 @@
+__author__ = 'DarkWing'
+
+import ntplib
+from time import ctime
+
+def print_time():
+    ntp_client = ntplib.NTPClient()
+    response = ntp_client.request("pool.ntp.org")
+    print ctime(response.tx_time)
+
+if __name__ == '__main__':
+    print_time()
