41a23c011c20b5a5139cd14bc10f39ca2f33a910
zjicmDarkWing <1126955201@qq.com>
2016-04-18 14:24:42 +0800

    packet sniffer

---
+++ 9/9_1_packet_sniffer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import argparse
+import pcap
+from construct.protocols.ipstack import ip_stack
+
+
+def print_packet(pktlen, data, timestamp):
+    if not data:
+        return
+
+    stack = ip_stack.parse(data)
+    payload = stack.next.next.next
+    print payload
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Packet Sniffer')
+    parser.add_argument('--iface', action="store", dest="iface", default='eth0')
+    parser.add_argument('--port', action="store", dest="port", default=80, type=int)
+    given_args = parser.parse_args()
+    iface, port = given_args.iface, given_args.port
+
+    pc = pcap.pcapObject()
+    pc.open_live(iface, 1600, 0, 100)
+    pc.setfilter('dst port %d' % port, 0, 0)
+
+    print 'Press CTRL+C to end capture'
+    try:
+        while True:
+            pc.dispatch(1, print_packet)
+    except KeyboardInterrupt:
+        print 'Packet statistics: %d packets received, %d packets dropped, %d packets dropped by the interface' % pc.stats()
+
+
+if __name__ == '__main__':
+    main()
